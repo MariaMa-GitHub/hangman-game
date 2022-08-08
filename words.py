1e458468cd82036d2621607fac7cbbeae4e76236
Maria Ma <maria.shurui.ma@gmail.com>
2022-08-07 20:52:47 -0400

    Edited Comment

--- words.py
+++ words.py
@@ -1,4 +1,4 @@
-# Random Words
+# random words
 word_list = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 'adjustment', 'advertisement', 'after',
              'again', 'against', 'agreement', 'air', 'all', 'almost', 'among', 'amount', 'amusement', 'and', 'angle',
              'angry', 'animal', 'answer', 'ant', 'any', 'apparatus', 'apple', 'approval', 'arch', 'argument', 'arm',
